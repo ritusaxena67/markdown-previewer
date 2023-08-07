39029901ad218485df0dc9b5b4d9c5e2cb5ea570
ritusaxena67 <ritudsaxena67@gmail.com>
2023-08-07 21:11:51 +0530

    Markdown Previewer Project Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { marked } from "marked";
+
+// defaultMarkdown contains valid markdown that represents at least one of each of the following elements: a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text
 
 function App() {
+  const [text, setText] = useState(`
+  # Welcome to my React Markdown Previewer!
+ ## This is a sub-heading...
+
+ [title](https://www.example.com)
+
+ \`<div></div>\`
+ \`\`\`
+// // this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+ if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+     return multiLineCode;
+   }
+ }
+ \`\`\`
+- First item
+- Second item
+- Third item
+
+> blockquote
+
+![alt text](image.jpg)
+
+**bold text**
+  `);
+
+  marked.setOptions({
+    breaks: true,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ textAlign: "center" }}>Markdown Previewer</h1>
+      <div className="boxes-container">
+        <textarea
+          name="editor"
+          id="editor"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{
+            __html: marked(text),
+          }}
+        />
+      </div>
+    </>
   );
 }
 
